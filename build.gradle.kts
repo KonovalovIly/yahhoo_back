3524ea43d4dda7961eaeb42b5fa989312ed528c7
KonovalovIly <konovalovily0987@gmail.com>
2022-11-10 09:37:43 +0400

    init

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
     kotlin("jvm") version "1.7.20"
     id("io.ktor.plugin") version "2.1.3"
     id("org.jetbrains.kotlin.plugin.serialization") version "1.7.20"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
 group = "ru.konovalovily"
@@ -27,8 +28,25 @@ dependencies {
     implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
     implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-cio-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     implementation("org.jsoup:jsoup:1.15.3")
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+}
+
+ktor {
+    docker {
+        jreVersion.set(io.ktor.plugin.features.JreVersion.JRE_17)
+        portMappings.set(listOf(
+            io.ktor.plugin.features.DockerPortMapping(
+                80,
+                8080,
+                io.ktor.plugin.features.DockerPortMappingProtocol.TCP
+            )))
+    }
+}
+
+tasks {
+    create("stage").dependsOn("installDist")
 }
--- src/main/kotlin/ru/konovalovily/Application.kt
+++ src/main/kotlin/ru/konovalovily/Application.kt
@@ -2,13 +2,15 @@ package ru.konovalovily
 
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
-import io.ktor.server.cio.*
+import io.ktor.server.netty.*
 import ru.konovalovily.data.api.MangaApiImpl
 import ru.konovalovily.data.repository.MangaRepositoryImpl
 import ru.konovalovily.plugins.*
 
 internal fun main() {
-    embeddedServer(CIO, port = 8080, host = "0.0.0.0", module = Application::module)
+    embeddedServer(Netty, port = System.getenv("PORT").toInt()){
+        module()
+    }
         .start(wait = true)
 }
 
