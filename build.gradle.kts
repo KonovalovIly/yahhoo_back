8ef34c7f84736b92f29199d79196397cc933561e
KonovalovIly <konovalovily0987@gmail.com>
2022-11-10 11:41:12 +0400

    doker

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,3 +34,16 @@ dependencies {
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
+
+ktor {
+    docker {
+        jreVersion.set(io.ktor.plugin.features.JreVersion.JRE_11)
+        portMappings.set(listOf(
+            io.ktor.plugin.features.DockerPortMapping(
+                80,
+                8080,
+                io.ktor.plugin.features.DockerPortMappingProtocol.TCP
+            )
+        ))
+    }
+}
--- src/main/kotlin/ru/konovalovily/Application.kt
+++ src/main/kotlin/ru/konovalovily/Application.kt
@@ -1,14 +1,14 @@
 package ru.konovalovily
 
 import io.ktor.server.application.*
+import io.ktor.server.cio.*
 import io.ktor.server.engine.*
-import io.ktor.server.netty.*
 import ru.konovalovily.data.api.MangaApiImpl
 import ru.konovalovily.data.repository.MangaRepositoryImpl
 import ru.konovalovily.plugins.*
 
 internal fun main() {
-    embeddedServer(Netty, port = 8080){
+    embeddedServer(CIO, port = 8080){
         module()
     }
         .start(wait = true)
