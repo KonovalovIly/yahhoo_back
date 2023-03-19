3589e007a70cc3ff97d876d2ed2e0a93a20f5155
KonovalovIly <konovalovily0987@gmail.com>
2023-03-19 22:47:36 +0400

    add downloadlink

--- src/main/kotlin/ru/konovalovily/domain/models/ChapterDto.kt
+++ src/main/kotlin/ru/konovalovily/domain/models/ChapterDto.kt
@@ -6,10 +6,16 @@ import ru.konovalovily.data.models.Chapter
 @Serializable
 internal data class ChapterDto(
     val id: String? = null,
+    val downloadLink: String? = null,
     val title: String? = null,
+    val date: String? = null,
+    val link: String? = null,
 )
 
 internal fun Chapter.mapToDomain() = ChapterDto(
     id = id,
+    downloadLink = downloadLink,
+    date = date,
+    link = link,
     title = title
 )
